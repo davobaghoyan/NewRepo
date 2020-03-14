5b37a22dc895f5fb2074ffd0f305662bc040a7fa
Davit Baghoyan <dbaghoyan@mail.ru>
2020-03-14 17:56:25 +0400

    pages

---
+++ Async_Await/Async_Await/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //ommitted for brevity
+            string message = await DoWorkAsync();
+            Console.WriteLine(message);
+            Console.WriteLine("GGG");
+            Console.ReadLine();
+            //ommitted for brevity
+        }
+        static string DoWork()
+        {
+            Thread.Sleep(5_000);
+            return "Done with work!";
+        }
+        static async Task<string> DoWorkAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(5_000);
+                return "Done with work!";
+            });
+        }
+      
+    }
+}
